3a84798b70640f9a6d695bf57b7254edb1854e32
ertorosyann <ertorosyann@gmail.com>
2025-04-28 13:47:18 +0400

    before adding functionality kind (Login, Register)

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,11 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+
+// import type { NextConfig } from "next";
+// import createNextIntlPlugin from "next-intl/plugin";
+
+// const nextConfig: NextConfig = {};
+
+// const withNextIntl = createNextIntlPlugin();
+// export default withNextIntl(nextConfig);
--- src/middleware.ts
+++ src/middleware.ts
@@ -14,3 +14,36 @@ export async function middleware(request: NextRequest) {
 export const config = {
   matcher: ["/admin/:path*"],
 };
+
+// import { NextRequest, NextResponse } from "next/server";
+// import createMiddleware from "next-intl/middleware";
+// import { routing } from "./i18n/routing";
+
+// // Создаем middleware для языка
+// const intlMiddleware = createMiddleware(routing);
+
+// export async function middleware(request: NextRequest) {
+//   // Сначала обрабатываем перевод
+//   const intlResponse = intlMiddleware(request);
+//   if (intlResponse instanceof NextResponse) {
+//     return intlResponse;
+//   }
+
+//   // Потом твоя проверка для /admin
+//   const adminToken = request.cookies.get("_token");
+//   const isLogin = Boolean(adminToken);
+
+//   if (request.nextUrl.pathname.startsWith("/admin") && !isLogin) {
+//     // ⚡ ВАЖНО: редирект должен учитывать язык!
+//     const locale = request.nextUrl.pathname.split("/")[1]; // например "en" или "hy"
+//     return NextResponse.redirect(new URL(`/${locale}/login`, request.url));
+//   }
+
+//   // Всё ок — продолжаем
+//   return NextResponse.next();
+// }
+
+// // Один правильный matcher
+// export const config = {
+//   matcher: ["/((?!api|trpc|_next|_vercel|.*\\..*).*)"],
+// };
